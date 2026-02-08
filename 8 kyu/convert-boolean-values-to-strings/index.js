19e3f663a05ff487f520b082323ef0d5ef272225
Danila Bochkarev <bockarevdanila174@gmail.com>
2026-02-08 18:43:08 +0300

    convert boolean

---
+++ 8 kyu/convert-boolean-values-to-strings/index.js
@@ -0,0 +1,9 @@
+// 3 Test Convert boolean values to strings 'Yes' or 'No'
+
+// Завершите разработку метода, который принимает логическое значение и возвращает строку "Yes" для значения true или строку "No" для значения false.
+
+function boolToWord( bool ){
+    return bool ? 'Yes' : 'No';
+  //...
+}
+console.log(boolToWord( -1))
